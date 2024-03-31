422ef6ebeafa3d0a8a87a9221d58b8b4ae8989ae
karlkaushalamanna1998 <karl@chipsy.in>
2024-03-31 22:49:33 +0530

    css chaneges

--- app/components/imageDIsplay/imageDisplay.tsx
+++ app/components/imageDIsplay/imageDisplay.tsx
@@ -3,32 +3,70 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-export default function ImageDisplay({ images , sticky }: { images: string[] , sticky : boolean }) {
+import { useEffect, useRef, useState } from 'react';
+export default function ImageDisplay({ images, sticky }: { images: string[], sticky: boolean }) {
+    const [viewingImage, setViewingImage] = useState("")
+
+    const imagesListRef : any = useRef(null);
+
+    const scrollDown = () => {
+        if (imagesListRef.current) {
+            imagesListRef.current.scrollTop += 90;
+        }
+    };
+
+    const scrollUp = () => {
+        if (imagesListRef.current) {
+            imagesListRef.current.scrollTop -= 90;
+        }
+    };
+
+    const nexImage = ()=>{
+        if(images.indexOf(viewingImage) == images.length - 1){
+            setViewingImage(images[0])
+        }else{
+            setViewingImage(images[images.indexOf(viewingImage) + 1])
+        }
+    }
+
+
+    const previousImage = ()=>{
+
+        if(images.indexOf(viewingImage) == 0){
+            setViewingImage(images[images.length - 1])
+
+        }else{
+            setViewingImage(images[images.indexOf(viewingImage) - 1])
+        }
+      }
+
+    useEffect(() => {
+        setViewingImage(images[0])
+    }, [images])
     return (
         <div className={sticky ? 'image_display_container sticky' : 'image_display_container'}>
             <div className='image_list_wrapper'>
-            <div className='images_list'>
-                {
-                    images.length > 0 && images.map((item , i)=>{
-                        return (
-                            <div key={i} className='single_Image_container'>
-                            <img src={'https://d22modxfw04m0o.cloudfront.net/' + item}/>
-                            
-                        </div>
-                        )
-                    })
-                }
+                <div className='images_list' ref={imagesListRef}>
+                    {
+                        images.length > 0 && images.map((item, i) => {
+                            return (
+                                <div key={i} className={viewingImage == item ? 'single_Image_container active' : 'single_Image_container'} onClick={()=>setViewingImage(item)}>
+                                    <img src={'https://d22modxfw04m0o.cloudfront.net/' + item} />
+                                </div>
+                            )
+                        })
+                    }
 
+                </div>
+                <button className='up_btn' onClick={scrollUp}><KeyboardArrowUpIcon /></button>
+                <button className='down_btn' onClick={scrollDown}><KeyboardArrowDownIcon /></button>
             </div>
-            <button className='up_btn'><KeyboardArrowUpIcon/></button>
-            <button className='down_btn'><KeyboardArrowDownIcon/></button>
-            </div>
-          
+
             {images.length > 0 && <div className='main_image'>
-            <button className='left_btn'><KeyboardArrowLeftIcon/></button>
-            <img src={'https://d22modxfw04m0o.cloudfront.net/' + images[0]}/>
-            <button className='right_btn'><KeyboardArrowRightIcon/></button>
-            <div className='count_display'>15/18</div>
+                <button className='left_btn' onClick={previousImage}><KeyboardArrowLeftIcon /></button>
+                {viewingImage && <img src={'https://d22modxfw04m0o.cloudfront.net/' + viewingImage} />}
+                <button className='right_btn' onClick={nexImage}><KeyboardArrowRightIcon /></button>
+                <div className='count_display'>{images.indexOf(viewingImage) + 1}/{images.length}</div>
             </div>}
         </div>
     );
