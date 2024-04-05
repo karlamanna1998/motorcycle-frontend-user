1588c3926374c7a046566a6783c4d414201c08d0
karlkaushalamanna1998 <karl@chipsy.in>
2024-04-06 00:36:00 +0530

    css fixes

--- app/components/navbar2/navbarTwo.jsx
+++ app/components/navbar2/navbarTwo.jsx
@@ -4,7 +4,19 @@ export default function NavbarTwo(){
     return (
         <nav className="nav2">
             <div className="nav2_inner">
-             navbar
+            <img className="logo" src="/logo3.png"/>
+
+            <div className="Search_container">
+            <div className="login_btn_container">
+            <img src="/login.png"/>
+            <span>Login</span>
+            </div>
+            <div className="login_btn_container">
+            <img src="/heart.png"/>
+            <span>Wistlist</span>
+            </div>
+            <input placeholder="Search"/>
+            </div>
             </div>
         </nav>
     )
